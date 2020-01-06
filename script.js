3855e8434557afafb1dcab750a9cffcb26347a52
Martin Schmal <martinschmal@gmail.com>
2020-01-06 14:34:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("Hello World!");
+
+//variable delacartion
+//let lets us reassign or change a variable
+let age;
+var city;
+
+city = "Berlin";
+
+//variable assignment
+age = 29;
+
+//variable initialization
+const firstName = "Martin";
+const sum = 1 + 1;
+
+//const cannot be reassigned
+//const lastName; Does not work
+//let age = 30; already declared
+//a variable declred with var or let cannot be redeclred
+//variable names hould be self descriptive and concise
+//Variable names should be lowercase or camelCase
+//UPPER case for variables that need to stand out
+
+// primitives| string boolean number undefinded null
+// Objects| array object
+
+// numbers represent numerice value
+const mileage = 100000; // integer
+const price = 15.99;
+//parseFloat(price) 15.99
+//parseInt(price) 15
+// % modulo ** exponent
+// Expression ist the combination of any values and operator that results in another value
+
+let x;
+x += 1; //x = x+1;
+
+const lastName = "schmal";
+const lastNames = 'schmal';
